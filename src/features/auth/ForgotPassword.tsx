719c60a3bb0784da0bef1afa070ab92f0d288b98
JoshX <joshx.dev@gmail.com>
2023-08-25 20:08:39 +0100

    feat: design forgot password modal

---
+++ src/features/auth/ForgotPassword.tsx
@@ -0,0 +1,26 @@
+import { FormInput } from '@/components';
+import { Modal } from '@/components/auth';
+import { Button, Link, Stack, Text, UseDisclosureProps } from '@chakra-ui/react';
+
+export const ForgotPassword = ({ isOpen = true, onClose = () => null }: UseDisclosureProps) => {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      modalHeader={
+        <Text mt={3} textStyle='body'>
+          Forogot Password
+        </Text>
+      }
+    >
+      <FormInput label='Email Address' placeholder='johndoe@email.com' />
+      <Link href='#' textStyle='body' fontWeight={400} mt={2}>
+        Reset Password with phone number
+      </Link>
+      <Stack spacing={3} my={3}>
+        <Button>Reset Password</Button>
+        <Button variant='text'>Login to your account</Button>
+      </Stack>
+    </Modal>
+  );
+};
