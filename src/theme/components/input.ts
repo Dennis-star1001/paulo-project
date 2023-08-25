775fe4bb405ce0001dda2f53a5060cd1088a21f9
JoshX <joshx.dev@gmail.com>
2023-08-25 15:36:22 +0100

    feat: style `input`

---
+++ src/theme/components/input.ts
@@ -0,0 +1,25 @@
+import { ThemeProps } from '@/theme/@types';
+
+export const input: ThemeProps = {
+  components: {
+    Input: {
+      variants: {
+        filled: {
+          field: {
+            border: '1px solid',
+            background: '#F1F5F9',
+            fontSize: '14px',
+            fontWeight: 400,
+            _hover: {
+              borderColor: 'primary'
+            }
+          }
+        }
+      },
+      defaultProps: {
+        variant: 'filled',
+        size: 'md'
+      }
+    }
+  }
+};
