181b63779599a0ddebb10b2c3e21789545625a8b
JoshX <joshx.dev@gmail.com>
2023-08-25 19:19:56 +0100

    feat: design the signup modal

--- src/features/auth/signup/Signup.tsx
+++ src/features/auth/signup/Signup.tsx
@@ -23,8 +23,16 @@ import {
   Text,
   UseDisclosureProps
 } from '@chakra-ui/react';
+import { ReactComponent as UserOctagonWhite } from '@/assets/user-octagon-white.svg';
+import { ReactComponent as UserCartWhite } from '@/assets/user-cart-white.svg';
+import { ReactComponent as UserOctagonBlack } from '@/assets/user-octagon-black.svg';
+import { ReactComponent as UserCartBlack } from '@/assets/user-cart-black.svg';
+import { useState } from 'react';
 
 export const SignupModal = ({ isOpen = true, onClose = () => null }: UseDisclosureProps) => {
+  const [tabIndex, setTabIndex] = useState(0);
+  const personalTabSelected = tabIndex === 0;
+
   return (
     <Modal isOpen={isOpen} onClose={onClose} size={['sm', 'lg']}>
       <ModalOverlay />
@@ -41,16 +49,45 @@ export const SignupModal = ({ isOpen = true, onClose = () => null }: UseDisclosu
           </Stack>
         </ModalHeader>
         <ModalBody px={[0, '22px']}>
-          <Tabs>
-            <TabList>
-              <Tab>Personal</Tab>
-              <Tab>Vendor</Tab>
-            </TabList>
+          <Tabs index={tabIndex} onChange={(index) => setTabIndex(index)}>
+            <Flex justifyContent='center'>
+              <TabList
+                as={Flex}
+                gap='18px'
+                border={0}
+                sx={{
+                  '.chakra-tabs__tab': {
+                    outline: 0,
+                    border: 0,
+                    w: '150px',
+                    h: '150px',
+                    color: '#000',
+                    _selected: {
+                      color: '#fff',
+                      bg: 'primary'
+                    }
+                  }
+                }}
+              >
+                <Tab>
+                  <Stack>
+                    {personalTabSelected ? <UserOctagonWhite /> : <UserOctagonBlack />}
+                    <Text>Personal</Text>
+                  </Stack>
+                </Tab>
+                <Tab>
+                  <Stack>
+                    {personalTabSelected ? <UserCartBlack /> : <UserCartWhite />}
+                    <Text>Vendor</Text>
+                  </Stack>
+                </Tab>
+              </TabList>
+            </Flex>
 
             <TabPanels>
               <TabPanel>
                 <Text textAlign='center'>Use Clerk here</Text>
-                <Divider />
+                <Divider my={8} />
 
                 <Stack spacing={4}>
                   <FormInput label='Email Address' placeholder='johndoe@email.com' />
@@ -83,7 +120,7 @@ export const SignupModal = ({ isOpen = true, onClose = () => null }: UseDisclosu
               </TabPanel>
               <TabPanel>
                 <Text textAlign='center'>Use Clerk here</Text>
-                <Divider />
+                <Divider my={8} />
                 <Grid templateColumns='repeat(2, 1fr)' rowGap={4} columnGap={5}>
                   <GridItem colSpan={2}>
                     <FormInput label='Company Name' placeholder='Cleaques Limited' />
@@ -122,7 +159,13 @@ export const SignupModal = ({ isOpen = true, onClose = () => null }: UseDisclosu
                     <FormInput label='Referral Code (optional)' />
                   </GridItem>
                 </Grid>
-                <Divider />
+                <Divider my='18px' />
+                <Stack spacing={1}>
+
+                <FormInput label='EIN/RIC Registration Number' />
+                <FormInput label='Upload business registration certificate' />
+                </Stack>
+                <Divider my='18px' />
                 <Stack spacing={2}>
                   <Checkbox>
                     <Text textStyle='body' fontWeight={400}>
