21653f5f0aa851fedf0d85eddcbb34d6a5e0e6d3
JoshX <joshx.dev@gmail.com>
2023-08-27 20:48:56 +0100

    misc

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 // import { EmailVerificationModal } from './features/auth';
 import { NewPasswordModal } from './features/auth';
 
-
 const App = () => {
   // const { isOpen, onOpen, onClose } = useDisclosure();
 
