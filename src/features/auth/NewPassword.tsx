b6eb3b04dcd36948830fafb93f7afc5ba0ea8898
JoshX <joshx.dev@gmail.com>
2023-08-27 20:45:25 +0100

    feat: new password modal

--- src/features/auth/NewPassword.tsx
+++ src/features/auth/NewPassword.tsx
@@ -1,6 +1,6 @@
-import { FormInput } from '@/components';
+import { PasswordInput } from '@/components';
 import { Modal } from '@/components/auth';
-import { Button, Link, Stack, Text, UseDisclosureProps } from '@chakra-ui/react';
+import { Button, Stack, Text, UseDisclosureProps } from '@chakra-ui/react';
 
 export const NewPasswordModal = ({ isOpen = true, onClose = () => null }: UseDisclosureProps) => {
   return (
@@ -9,10 +9,10 @@ export const NewPasswordModal = ({ isOpen = true, onClose = () => null }: UseDis
       onClose={onClose}
       modalHeader={<Text textStyle='body'> New Password </Text>}
     >
-      <Link href='#' textStyle='body' fontWeight={400} mt={2}>
+      <Text color='primary' textStyle='body-regular' fontWeight={400} mt={2}>
         Enter a new password that you can remember to access your account
-      </Link>
-      <FormInput placeholder='Password' type='password'/>
+      </Text>
+      <PasswordInput type='password' />
       <Stack mt={7}>
         <Button>Reset Password</Button>
       </Stack>
