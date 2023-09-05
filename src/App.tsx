08037329b033993f15c91d5c8a010e555b3cb4f4
JoshX <joshx.dev@gmail.com>
2023-09-05 13:39:37 +0100

    config: setup basic routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { path } from './routes/path';
 import { ProductPage } from './pages';
 
+const router = createBrowserRouter([
+  {
+    path: path.HOME,
+    element: <div>Home page goes here</div>
+  },
+  {
+    path: path.CHAT,
+    element: <div>Chat page goes here</div>
+  },
+  {
+    path: path.DONATIONS,
+    element: <div>Donation page goes here</div>
+  },
+  {
+    path: path.SHOP,
+    element: <div>Shop page goes here</div>
+  },
+  {
+    path: path.EVENTS,
+    element: <div>Events page goes here</div>
+  },
+  {
+    path: path.PRODUCT,
+    element: <ProductPage />
+  }
+]);
 const App = () => {
   // const { isOpen, onOpen, onClose } = useDisclosure();
 
-  return (
-    <>
-      <ProductPage />
-    </>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
---
+++ src/routes/path.ts
@@ -0,0 +1,9 @@
+export const path = {
+  HOME: '/',
+  CHAT: '/chat',
+  SHOP: '/shop',
+  DONATIONS: '/donations',
+  EVENTS: '/events',
+  SOCIAL: '/social',
+  PRODUCT: '/product'
+};
