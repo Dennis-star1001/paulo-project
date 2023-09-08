4da50c2d938701eea26e7913b666913518c7b0ce
vontek <1704devops@gmail.com>
2023-09-08 19:23:18 +0100

    hompage/nav-bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 // import { EmailVerificationModal } from './features/auth';
 // import { NewPasswordModal } from './features/auth';
-import { LoginModal } from './features/auth';
+import BeforeLoginHeader from './components/Header/BeforeLoginHeader';
+import AfterLoginHeader from './components/Header/AfterLoginHeader';
+//import { LoginModal } from './features/auth';
 
 const App = () => {
   // const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <>
-      <LoginModal />
+      {/* <LoginModal /> */}
+      <BeforeLoginHeader />
+      {/* <AfterLoginHeader/> */}
     </>
   );
 };
