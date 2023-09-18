65606cab2fdeed2fd40771bc254334068115777a
JoshX <joshx.dev@gmail.com>
2023-09-18 07:27:55 +0100

    add gossips routes

--- src/routes/path.ts
+++ src/routes/path.ts
@@ -7,6 +7,7 @@ export const path = {
   EVENTS_DETAILS: '/event_details',
   SOCIAL: '/social',
   SOCIAL_DETAIL: '/social/:id',
+  GOSSIP: '/gossips',
   PRODUCT: '/product',
   SERVICES: '/services',
   SERVICES_DETAIL: '/services/:id'
