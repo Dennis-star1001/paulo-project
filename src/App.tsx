0b296dc1d937e4dd266d82d675d2313ea3495712
JoshX <joshx.dev@gmail.com>
2023-09-19 15:48:36 +0100

    wip: add gossip post

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-// import { RouterProvider } from 'react-router-dom';
-// import BeforeLoginHeader from './components/Header/BeforeLoginHeader';
-// import Navigation from './components/Navigation/Navigation';
-// import { router } from './routes';
-// import BeforeLoginFooter from './components/Footer/BeforeLoginFooter';
-import { GossipPage } from './features/auth';
+import { RouterProvider } from 'react-router-dom';
+import BeforeLoginHeader from './components/Header/BeforeLoginHeader';
+import Navigation from './components/Navigation/Navigation';
+import { router } from './routes';
+import BeforeLoginFooter from './components/Footer/BeforeLoginFooter';
 
 const App = () => {
   return (
     <>
-      {/* <BeforeLoginHeader />
+      <BeforeLoginHeader />
       <Navigation />
       <RouterProvider router={router} />
-      <BeforeLoginFooter /> */}
-      <GossipPage/>
+      <BeforeLoginFooter />
     </>
   );
 };
--- src/pages/gossip/Gossip.tsx
+++ src/pages/gossip/Gossip.tsx
@@ -1,3 +1,83 @@
+import { IconButton } from '@/components';
+import { Flex, Grid, GridItem, HStack, Stack, Text } from '@chakra-ui/react';
+import { RiAddCircleFill, RiSearchLine } from 'react-icons/ri';
+import { SocialCard } from '../social/components';
+
+const socials = [
+  {
+    img: 'https://www.johnmeyerwebdev.com/images/Me-Avatar-Maker.svg',
+    name: 'Mike John',
+    time: '2hrs',
+    taggedName: 'Orioferoliwa Peters',
+    comment: `Sed elementum turpis netus maecenas et quis neque feugiat. Dolor purus egestas hac
+    adipiscing mauris convallis sed. Mattis a rhoncus vulputate elit blandit egestas feugiat sed
+    augue. Tempor commodo risus lacus nec purus dictum dolor.`,
+    likes: 16,
+    comments: 9,
+    hasImg: true,
+    numberOfComments: 10
+  },
+  {
+    img: 'https://www.johnmeyerwebdev.com/images/Me-Avatar-Maker.svg',
+    name: 'Mike John',
+    time: '2hrs',
+    taggedName: 'Orioferoliwa Peters',
+    comment: `Sed elementum turpis netus maecenas et quis neque feugiat. Dolor purus egestas hac
+    adipiscing mauris convallis sed. Mattis a rhoncus vulputate elit blandit egestas feugiat sed
+    augue. Tempor commodo risus lacus nec purus dictum dolor.`,
+    likes: 16,
+    comments: 9,
+    hasImg: false,
+    numberOfComments: 10
+  },
+  {
+    img: 'https://www.johnmeyerwebdev.com/images/Me-Avatar-Maker.svg',
+    name: 'Mike John',
+    time: '2hrs',
+    taggedName: 'Orioferoliwa Peters',
+    comment: `Sed elementum turpis netus maecenas et quis neque feugiat. Dolor purus egestas hac
+    adipiscing mauris convallis sed. Mattis a rhoncus vulputate elit blandit egestas feugiat sed
+    augue. Tempor commodo risus lacus nec purus dictum dolor.`,
+    likes: 16,
+    comments: 9,
+    hasImg: true,
+    numberOfComments: 10
+  }
+];
+
 export const GossipPage = () => {
-  return <div>Gossip</div>;
+  return (
+    <Grid templateColumns='repeat(12, 1fr)' rowGap='22px' columnGap='36px'>
+      <GridItem colSpan={[12, 7]}>
+        <Flex justifyContent='space-between' mb={6}>
+          <Text as='h1' fontSize='40px' fontWeight={600}>
+            Gossip
+          </Text>
+          <HStack>
+            <RiSearchLine />
+            <IconButton display={['none', 'block']} icon={<RiAddCircleFill />}>
+              Create Post
+            </IconButton>
+          </HStack>
+        </Flex>
+        <Stack spacing={2}>
+          {socials.map(
+            ({ comment, img, likes, name, numberOfComments, taggedName, time, hasImg }, i) => (
+              <SocialCard
+                comment={comment}
+                img={img}
+                likes={likes}
+                name={name}
+                numberOfComments={numberOfComments}
+                taggedName={taggedName}
+                time={time}
+                hasImg={hasImg}
+                key={i}
+              />
+            )
+          )}
+        </Stack>
+      </GridItem>
+    </Grid>
+  );
 };
