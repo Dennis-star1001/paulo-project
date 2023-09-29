112b3f17509366f50b6804c41963d37e5686b109
vontek <1704devops@gmail.com>
2023-09-29 19:36:02 +0100

    VontekX/Vendor-page

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,32 @@ import BeforeLoginHeader from './components/Header/BeforeLoginHeader';
 import Navigation from './components/Navigation/Navigation';
 import { router } from './routes';
 import BeforeLoginFooter from './components/Footer/BeforeLoginFooter';
+import Dashboard from './pages/vendor/dashboard/Dashboard';
+import Services from './pages/vendor/dashboard/services/Services';
+import Donation from './pages/vendor/dashboard/donation/Donation';
+import Analytics from './pages/vendor/dashboard/analysis/Analytics';
+import Events from './pages/vendor/event/Events';
+import EventDetails from './pages/vendor/event/Eventdetails';
+import EventLive from './pages/vendor/event/Eventlive';
+import CreateEvent from './pages/vendor/event/createEvent';
 
 const App = () => {
   return (
     <>
-      <BeforeLoginHeader />
+      {/* <BeforeLoginHeader />
       <Navigation />
       <RouterProvider router={router} />
-      <BeforeLoginFooter />
+      <BeforeLoginFooter /> */}
+      <>
+        <Dashboard/>
+        {/* <Services/> */}
+        {/* <Donation/> */}
+        {/* <Analytics/> */}
+        {/* <Events/> */}
+        {/* <EventDetails/> */}
+        {/* <EventLive/> */}
+        {/* <CreateEvent/> */}
+      </>
     </>
   );
 };
