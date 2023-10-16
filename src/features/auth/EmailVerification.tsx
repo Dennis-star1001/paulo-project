f440aefa6ffb2bc24b8f4093532a7ede2eabe40f
JoshX <joshx.dev@gmail.com>
2023-10-16 12:28:58 +0100

    feat: email verification form setup

--- src/features/auth/EmailVerification.tsx
+++ src/features/auth/EmailVerification.tsx
@@ -1,4 +1,8 @@
+import { useAppDispatch, useAppSelector } from '@/app/hooks';
+import { useVerifyPasswordCodeMutation } from '@/app/services/auth';
+import { setForgotEmailToken } from '@/app/slice/authSlice';
 import { Modal } from '@/components/auth';
+import { useHandleError, useHandleSuccess } from '@/hooks';
 import {
   Button,
   HStack,
@@ -8,11 +12,52 @@ import {
   Text,
   UseDisclosureProps
 } from '@chakra-ui/react';
+import { Form, FormikProvider, useFormik } from 'formik';
+import { VerifyCodeFormValues, VerifyCodeSchema } from './schema/auth.schema';
 
+type EmailVerificationModalProps = UseDisclosureProps & {
+  onLogin?: () => void;
+};
 export const EmailVerificationModal = ({
   isOpen = true,
-  onClose = () => null
-}: UseDisclosureProps) => {
+  onClose = () => null,
+  onLogin = () => null
+}: EmailVerificationModalProps) => {
+  const [verifyCode] = useVerifyPasswordCodeMutation();
+
+  const handleError = useHandleError();
+  const handleSuccess = useHandleSuccess();
+
+  const dispatch = useAppDispatch();
+
+  const email = useAppSelector((state) => state.auth?.forgotEmail);
+
+  const formik = useFormik<VerifyCodeFormValues>({
+    initialValues: {
+      email: '',
+      token: ''
+    },
+    onSubmit: async (values) => {
+      try {
+        const response = await verifyCode({
+          email,
+          token: values.token
+        }).unwrap();
+        const status = response.status;
+
+        handleSuccess('', response.message, status);
+        if (status) {
+          onClose();
+          dispatch(setForgotEmailToken(values.token));
+        }
+      } catch (err) {
+        handleError(err);
+      }
+    },
+    validationSchema: VerifyCodeSchema
+  });
+  const { values, isSubmitting, setFieldValue } = formik;
+
   return (
     <Modal
       isOpen={isOpen}
@@ -24,33 +69,49 @@ export const EmailVerificationModal = ({
         password
       </Text>
 
-      <HStack
-        sx={{
-          '.chakra-pin-input': {
-            bg: '#E6F2F7',
-            border: '1px solid !important',
-            borderColor: '#fff !important',
-            borderRadius: '6px',
-            _placeholder: { fontSize: '26px', color: 'gray.300' },
-            h: '64px',
-            w: '64px'
-          }
-        }}
-      >
-        <PinInput mask placeholder='' variant='flushed' focusBorderColor='transparent'>
-          <PinInputField />
-          <PinInputField />
-          <PinInputField />
-          <PinInputField />
-          <PinInputField />
-          <PinInputField />
-          <PinInputField />
-        </PinInput>
-      </HStack>
-      <Stack spacing={3} my={3}>
-        <Button>Reset Password</Button>
-        <Button variant='text'>Login to your account</Button>
-      </Stack>
+      <FormikProvider value={formik}>
+        <Form>
+          <HStack
+            sx={{
+              '.chakra-pin-input': {
+                bg: '#E6F2F7',
+                border: '1px solid !important',
+                borderColor: '#fff !important',
+                borderRadius: '6px',
+                _placeholder: { fontSize: '26px', color: 'gray.300' },
+                h: '64px',
+                w: 'full'
+              }
+            }}
+          >
+            <PinInput
+              type='alphanumeric'
+              mask
+              placeholder=''
+              variant='flushed'
+              focusBorderColor='transparent'
+              id='token'
+              value={values.token}
+              onChange={(_code) => setFieldValue('token', _code)}
+            >
+              <PinInputField />
+              <PinInputField />
+              <PinInputField />
+              <PinInputField />
+              <PinInputField />
+              <PinInputField />
+            </PinInput>
+          </HStack>
+          <Stack spacing={3} my={3}>
+            <Button isDisabled={isSubmitting} type='submit'>
+              Reset Password
+            </Button>
+            <Button onClick={onLogin} variant='text'>
+              Login to your account
+            </Button>
+          </Stack>
+        </Form>
+      </FormikProvider>
     </Modal>
   );
 };
